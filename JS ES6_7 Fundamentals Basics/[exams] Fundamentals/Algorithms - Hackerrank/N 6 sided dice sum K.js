2d8e72d416e97d63d41d52e8d099294b4e8c46d5
starazagora010 <starazagora010@gmail.com>
2019-09-29 17:14:52 +0000

    Gitpod online dev-ing test

---
+++ JS ES6_7 Fundamentals Basics/[exams] Fundamentals/Algorithms - Hackerrank/N 6 sided dice sum K.js
@@ -0,0 +1,15 @@
+function solve(arg){
+    let args = arg.split(' ').map(Number);
+    let dices = args[0];
+    let sides = args[1];
+    let nDicestoSum = args[2];
+    let sum = args[3];
+
+    //permutations and variations are possible
+}
+
+solve(
+    "5 6 3 15"
+);
+
+// https://medium.com/gitpod/debugging-node-js-applications-in-theia-76c94c76f0a1
--- JS ES6_7 Fundamentals Basics/[exams] Fundamentals/quicktest.js
+++ JS ES6_7 Fundamentals Basics/[exams] Fundamentals/quicktest.js
@@ -1,1 +1,6 @@
-console.log("hgggi");
+arg =  "5 6 3 15";
+let args = arg.split(' ').map(Number);
+
+console.log(args)
+
+console.log(typeof(args[0]))
